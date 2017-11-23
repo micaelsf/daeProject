f05db257a14fdf01775c64181b9fe37ced2b754d
Bruno Alves <bgualter@gmail.com>
2017-11-23 01:17:48 +0000

    Instituitions resolved escaravelho on show data on admin/index

--- DaeProject/DaeProject-ejb/src/java/ejbs/InstituitionBean.java
+++ DaeProject/DaeProject-ejb/src/java/ejbs/InstituitionBean.java
@@ -24,7 +24,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Stateless
-@Path("/instituition")
+@Path("/instituitions")
 public class InstituitionBean {
 
     @PersistenceContext
